5cc4f516196d401735eb78fdf5997a76490185a5
Jeronimo@lola <jggrandi@gmail.com>
2016-09-10 17:02:05 -0300

    C script to stract roles finished. aaa.csv was generated with the role of each team member

--- Analysis/C/CountChanges/main.cpp
+++ Analysis/C/CountChanges/main.cpp
@@ -4,16 +4,13 @@ using namespace std;
 
 int main(){
     int considerChange = 4;
-    int stillSameRole = 0;
-    int input = -1;
-    int valueNow = -1;
-    int countChanges = 0;
-    int member;
-    int count = 0;
-    int temp = -1;
-    while(member != 99){
-        cin >> member;
-        if(member == 99) break;
+    int input, valueNow = -1;
+    int countChanges,userID,count,temp=0;
+
+    cout << "UserID;Changes" <<endl;
+    while(userID != 99){
+        cin >> userID;
+        if(userID == 99) break;
         while(input != 0){
             cin >> input;
             if (input == 0){
@@ -35,12 +32,13 @@ int main(){
             }
 
         }
-        cout << "Changes by Member " << member << ": " << countChanges << endl;
+        cout << userID << ";" << countChanges << endl;
         countChanges = 0;
         count = 0;
         temp = -1;
+        valueNow = -1;
     }
-    cout << "END" << endl;
+
     return 0;
 
 }
